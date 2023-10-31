fa3bf44eecd9129e515cc59381519f4d5efec3d4
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-10-31 16:41:02 -0300

    feat: add try catch

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿public class Program
+﻿namespace consoleException
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            var arr = new int[3];
+            try
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    //System.IndexOutOfRangeException
+                    Console.WriteLine(arr[i]);
+                }
+            } 
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Índice não encontrado");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Souce: " + e.Source);
+                Console.WriteLine("Type: " + e.GetType());
+                Console.WriteLine("Message: " + e.Message);
+                Console.WriteLine("StackTrace: " + e.StackTrace);
+            }
+        }
     }
 }
